c59fd079e44b19da9cb990b26e1600432a0678c9
zafarabdullayev <Zafarjon_Abdullaev@student.itpu.uz>
2026-03-31 13:58:25 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
+-- Tables
+CREATE TABLE clinic.patients (...);
 
+CREATE TABLE clinic.doctors (...);
+
+CREATE TABLE clinic.departments (...);
+
+-- Many-to-many
+CREATE TABLE clinic.doctor_department (...);
+
+-- Data insert 
+INSERT INTO clinic.patients ...;
+
+INSERT INTO clinic.doctors ...;
+
+INSERT INTO clinic.departments ...;
+
+-- RELATION INSERT 
+INSERT INTO clinic.doctor_department
+SELECT ...
+
+-- record_ts
+ALTER TABLE clinic.patients ADD COLUMN record_ts DATE DEFAULT CURRENT_DATE;
